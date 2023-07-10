eb6d0e683bc4bf64decdf013db38e6bf1dec3b61
bibiknv <138819017+bibiknv@users.noreply.github.com>
2023-07-10 16:20:04 +0300

    task4 passed

--- Python_for_prof/10.07/task4/main.py
+++ Python_for_prof/10.07/task4/main.py
@@ -1,9 +1,24 @@
-# Для тех, кому сложно как было и мне, вот простое решение:
-#
-# отсортировать прочтенный файл по дате и потом  добавлять вот в такой словарь:
-#
-# "ключ-имейл": [ник, имейл, дата]
-#
-# В итоге ключ может быль только один, а значение будет перезаписано из за отсортированости по дате и попадать последняя запись.
-# 
-# При выводе в файл, снова сортировать, но теперь по имени
+
+
+import csv
+
+with open('name_log.csv', encoding='utf-8') as file:
+    data = file.read()
+    rows = [r.split(',') for r in data.splitlines()]
+    first_row = rows[0]
+    del rows[0]
+    expensive = sorted(rows, key=lambda item: item[2])
+    result_dict = {}
+    for row in expensive:
+        if row[1] not in result_dict:
+            result_dict[row[1]] = list(row)
+        else:
+            result_dict[row[1]] = list(row)
+
+with open('new_name_log.csv', 'w', encoding='utf-8', newline='') as file:
+    writer = csv.writer(file, delimiter=',')
+    writer.writerow(first_row)
+    for v in sorted(result_dict.values(), key=lambda v: v[1]):
+        writer.writerow(v)
+
+
