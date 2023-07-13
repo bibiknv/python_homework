7afbdcf561a711bdfc4f66b853a57c050e3e40d8
bibiknv <138819017+bibiknv@users.noreply.github.com>
2023-07-13 20:19:27 +0300

    task2 passed

--- Python_for_prof/11.07/task2/main.py
+++ Python_for_prof/11.07/task2/main.py
@@ -1,25 +1,19 @@
-
 import csv
 
 with open('prices.csv', 'r', encoding='utf-8') as file:
     reader = csv.DictReader(file, delimiter=';')
-    result_list = []
 
     res_dict = {}
     for i, row in enumerate(reader):
         for key, value in row.items():
             if key != 'Магазин':
                 res_dict.setdefault(row['Магазин'], []).append((key, value))
-    #         if key == 'Магазин':
-    #             new_dict[value] = []
-    print(res_dict)
 
+    result_list = []
+    for key, value in res_dict.items():
+        min_value = list(min(value, key=lambda x: (int(x[1]), x[0])))
+        min_value.append(key)
+        result_list.append(min_value)
+    min_price_product = min(result_list, key=lambda x: (int(x[1]), x[0], x[2]))
+    print(min_price_product[0], ': ', min_price_product[2], sep='')
 
-    #for row in reader:
-    #     result_list.append(row)
-
-# with open('prices_result.csv', 'w', encoding='utf-8', newline='') as file:
-#     writer = csv.DictWriter(file, fieldnames=file_names, delimiter=';')
-#     writer.writeheader()
-#     for row_new in result_list:
-#         writer.writerow(row_new)
