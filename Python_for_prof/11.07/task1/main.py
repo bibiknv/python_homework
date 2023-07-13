2c78f605b38ea9d2c0d9b0ab5456cb7f40066334
bibiknv <138819017+bibiknv@users.noreply.github.com>
2023-07-13 14:41:15 +0300

    task1 passed

--- Python_for_prof/11.07/task1/main.py
+++ Python_for_prof/11.07/task1/main.py
@@ -1,32 +1,20 @@
 import csv
+
 with open('student_counts.csv', 'r', encoding='utf-8') as file:
-    reader = list(csv.DictReader(file, delimiter=','))
-    file_names = [i for i in reader[0]]
-    sorted_file_names = sorted(file_names, key = lambda x: (len(x), x))
-    print(reader)
+    reader = csv.DictReader(file, delimiter=',')
+
+    file_names = reader.fieldnames
+    file_names_sorted = sorted(file_names[1:], key=lambda x: (len(x), x))
+    first_string_names = file_names[:1] + file_names_sorted
+
+    result_list = []
+    for row in reader:
+        result_list.append(row)
+
 
 with open('sorted_student_counts.csv', 'w', encoding='utf-8', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(sorted_file_names)
-    for row in reader:
-        for s in row:
-            writer.writerow(row)
-
-    # reader.fieldnames
-    # result = []
-    # result_list = list(reader)
-    # for dct_i in result_list:
-    #     print(type(dct_i))
-        # dct_i = sorted(dct_i, key = lambda x: (len(x), x))
-        # print(type(dct_i))
-
-
-
-
-# with open('student_counts.csv', 'r', encoding='utf-8') as file:
-#     rows = list(csv.DictReader(file, delimiter=','))
-#     colums = [i for i in rows[0]]
-#     for obj in rows:
-
-
+    writer = csv.DictWriter(file, fieldnames=first_string_names, delimiter=',')
+    writer.writeheader()
+    for row_new in result_list:
+        writer.writerow(row_new)
 
