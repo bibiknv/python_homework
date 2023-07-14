1be9817a930c7a2d854b28b832aeb1c6642a244b
bibiknv <138819017+bibiknv@users.noreply.github.com>
2023-07-14 19:44:22 +0300

    passed task1 in pandas

---
+++ Python_for_prof/13.07/pandas_learning/main.py
@@ -0,0 +1,26 @@
+#import pandas as pd
+# my_series2 = pd.Series([5, 6, 7, 8, 9, 10], index=['a', 'b', 'c', 'd', 'e', 'f'])
+# print(my_series2['f'])
+
+# import pandas as pd
+# my_series3 = pd.Series({'a': 5, 'b': 6, 'c': 7, 'd': 8})
+# my_series3.name = 'numbers'
+# my_series3.index.name = 'letters'
+# print(my_series3)
+#
+
+# import pandas as pd
+# df = pd.DataFrame({
+#     'country': ['Kazakhstan', 'Russia', 'Belarus', 'Ukraine'],
+#     'population': [17.04, 143.5, 9.5, 45.5],
+#     'square': [2724902, 17125191, 207600, 603628]
+# })
+#
+# print(df)
+#
+#
+# import pandas as pd
+#
+# list_in = [1, 2, 3, 4, 5]
+# my_series = pd.Series([1, 2, 3, 4, 5])
+# print(my_series)
