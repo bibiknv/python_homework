a84bcc0c3155545d35c2200e0a1393c87e5f572c
bibiknv <138819017+bibiknv@users.noreply.github.com>
2023-07-19 16:28:34 +0300

    passed tasks in pandas

---
+++ Python_for_prof/19.07/task1/main.py
@@ -0,0 +1,13 @@
+import json
+
+with open('data1.json', encoding='utf-8') as file:
+    data_first = json.load(file)
+
+with open('data2.json', encoding='utf-8') as file:
+    data_second = json.load(file)
+
+data_3 = data_first | data_second
+
+with open('data_merge.json', 'w') as file:
+    json.dump(data_3, file, indent=3)
+
