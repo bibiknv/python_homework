7b03fd65eb95e77fb6c9b58425faa4c3055efe6b
bibiknv <138819017+bibiknv@users.noreply.github.com>
2023-07-25 13:04:29 +0300

    passed tasks

---
+++ Python_for_prof/25.07/task2/main.py
@@ -0,0 +1,12 @@
+from zipfile import ZipFile
+
+file_names = ['how to prove.pdf', 'fipi_demo_2022.pdf', 'Hollow Knight Silksong.exe',
+              'code.jpeg', 'stepik.png', 'readme.txt', 'shopping_list.txt',
+              'Alexandra Savior – Crying All the Time.mp3', 'homework.py', 'test.py']
+
+from zipfile import ZipFile
+
+with ZipFile('files.zip', mode='w') as zip_file:
+    for name in file_names:
+        zip_file.write(name)
+print(zip_file.namelist())
