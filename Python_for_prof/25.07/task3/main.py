38fbc03a1764748a0b1455acc3018615c567bb67
bibiknv <138819017+bibiknv@users.noreply.github.com>
2023-07-25 14:54:53 +0300

    passed tasks

---
+++ Python_for_prof/25.07/task3/main.py
@@ -0,0 +1,12 @@
+import os.path
+from zipfile import ZipFile
+
+file_names = ['code.jpeg', 'stepik.png', 'readme.txt', 'shopping_list.txt',
+              'Alexandra Savior – Crying All the Time.mp3', 'homework.py', 'test.py']
+
+with ZipFile('test.zip', mode='a') as zip_file:
+    for name in file_names:
+        size = os.path.getsize(name)
+        if size <= 100:
+            zip_file.write(name)
+
