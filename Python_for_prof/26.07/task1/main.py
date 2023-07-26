285b09eeb5fbb707c8b14d306b7d10199d3224ea
bibiknv <138819017+bibiknv@users.noreply.github.com>
2023-07-26 17:19:05 +0300

    passed tasks

---
+++ Python_for_prof/26.07/task1/main.py
@@ -0,0 +1,11 @@
+
+
+def convert_bytes(size):
+    if size < 1000:
+        return f'{size} B'
+    elif 1000 <= size < 1000000:
+        return f'{round(size / 1024)} KB'
+    elif 1000000 <= size < 1000000000:
+        return f'{round(size / 1048576)} MB'
+    else:
+        return f'{round(size / 1073741824)} GB'
