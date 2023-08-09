7613c89ae2130c3969b2199cc00780185b89ffae
bibiknv <138819017+bibiknv@users.noreply.github.com>
2023-08-09 16:00:17 +0300

    passed tasks

---
+++ Python_for_prof/08.08/task4/main.py
@@ -0,0 +1,6 @@
+from collections import OrderedDict
+
+data = OrderedDict(Dustin=29, Anabel=17, Brian=40, Carol=16)
+custom_sort(data)
+
+print(data)
---
+++ Python_for_prof/09.08/task2/main.py
@@ -0,0 +1,5 @@
+word = 'python'
+words = 'Python Conferences python training python events'
+
+print(count_occurences(word, words))
+
